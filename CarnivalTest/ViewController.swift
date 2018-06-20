2b0326f9de910ad5af3bad7c109b64f0e9d93076
aaron g <aaron.gray@avatarlabs.com>
2018-06-20 13:43:11 -0700

    Implementing game plane selection & excess anchor removal

--- CarnivalTest/ViewController.swift
+++ CarnivalTest/ViewController.swift
@@ -14,27 +14,31 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     
+    var selectedAnchor: ARAnchor? = nil;
+    var anchors: [ARAnchor] = [ARAnchor]()
+    
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//~~~~~APP STATE CONTROL~~~~~~~~~~~~~~~~
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Set the view's delegate
         sceneView.delegate = self
         
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
+        let tapRecognizer = UITapGestureRecognizer(target: self, action:#selector(handleTap(rec:)))
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
+        sceneView.addGestureRecognizer(tapRecognizer)
+
     }
-    
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = [.vertical]
 
         // Run the view's session
         sceneView.session.run(configuration)
@@ -47,16 +51,129 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.pause()
     }
 
-    // MARK: - ARSCNViewDelegate
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//~~~~AR ANCHORS~~~~~~~~~~~~~~~~~~~~~~~~
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        
+        
+        guard let planeAnchor = anchor as? ARPlaneAnchor else {return}
+        
+//        Add spherical marker
+        let marker = SCNSphere(radius: 0.01)
+        let markerNode = SCNNode(geometry: marker)
+        markerNode.simdPosition = float3(planeAnchor.center.x, 0, planeAnchor.center.z)
+        markerNode.opacity = 0.8
+        
+//        Add planar extent indicator
+        let plane = SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z))
+        let planeNode = SCNNode(geometry: plane)
+        planeNode.simdPosition = float3(planeAnchor.center.x, 0, planeAnchor.center.z)
+        planeNode.opacity = 0.25
+        planeNode.eulerAngles.x = -.pi / 2
+        
+//        Add visual indicator nodes to anchor node
+        node.addChildNode(markerNode)
+        node.addChildNode(planeNode)
+
+//        Store ref to anchor
+        self.anchors.append(anchor)
     }
-*/
+    
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+
+        guard let planeAnchor = anchor as? ARPlaneAnchor,
+            let markerNode: SCNNode = node.childNodes[0],
+            let planeNode: SCNNode = node.childNodes[1],
+              let plane = planeNode.geometry as? SCNPlane
+              else {return}
+        
+//        Update visual indicators
+        markerNode.simdPosition = float3(planeAnchor.center.x, 0, planeAnchor.center.z)
+        planeNode.simdPosition = float3(planeAnchor.center.x, 0, planeAnchor.center.z)
+        plane.width = CGFloat(planeAnchor.extent.x)
+        plane.height = CGFloat(planeAnchor.extent.z)
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didRemove node: SCNNode, for anchor: ARAnchor) {
+        print("Removing Anchor")
+        
+//        Remove ref to selected anchor
+        self.anchors.removeAll(where: { $0.identifier == anchor.identifier})
+    }
+    
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//~~~~~~AR SESSION CONTROLS~~~~~~~~~~~~~
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    
+    @objc func handleTap(rec: UITapGestureRecognizer){
+        
+//        Tap ended
+        if rec.state == .ended {
+            
+//            Onscreen tap position
+            let tapLocation: CGPoint = rec.location(in: sceneView)
+            
+//            Get hitscan from inside anchor's extents
+            let hitTestResults = self.sceneView.hitTest(tapLocation, types: [.existingPlaneUsingExtent])
+            
+            
+//            No results found
+            if hitTestResults.isEmpty {
+                print("No hits on tap")
+                return
+            }
+            
+//            If only 1 hit found
+            if hitTestResults.count == 1 {
+                self.selectedAnchor = hitTestResults.first!.anchor!
+                
+                print("Anchor selected: ")
+                print(self.selectedAnchor)
+                
+            }
+//            Otherwise get closest hit ( a precaution in case of overlap )
+            else {
+                var closestDistance: CGFloat?;
+                for result in hitTestResults {
+                    if closestDistance == nil {
+                        closestDistance = result.distance
+                    }
+                    if result.distance < closestDistance! {
+                        closestDistance = result.distance
+                        self.selectedAnchor = result.anchor!
+                    }
+                }
+                
+//                Remove nonselected anchors from session
+                for i in 0...self.anchors.count-1 {
+                    if self.anchors[i].identifier != self.selectedAnchor?.identifier {
+                        self.sceneView.session.remove(anchor: self.anchors[i])
+                    }
+                }
+            }
+            
+//            We've selected our play surface so no need to keep making new anchors
+            disablePlaneDetection()
+        }
+    }
+    
+//    Restart session w/ selective plane detection
+    func enablePlaneDetection(types: ARWorldTrackingConfiguration.PlaneDetection) {
+        let config = ARWorldTrackingConfiguration()
+        config.planeDetection = types
+        
+        sceneView.session.run(config)
+    }
+    
+//    Restart session w/ no plane detection
+    func disablePlaneDetection() {
+        let config = ARWorldTrackingConfiguration()
+        config.planeDetection = []
+        
+        sceneView.session.run(config)
+    }
     
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
